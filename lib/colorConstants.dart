9283032cd76fadcccf06cf13dcbebfc776f3572f
janWICKEDpro <janroyal45@gmail.com>
2020-09-17 04:12:01 +0100

    colorThemes and sizes

---
+++ lib/colorConstants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color primaryTheme = new Color(0xff98c429);
+Color submitButtonColor = new Color(0xff9c27b0);
+Color other = new Color(0xff00d3ee);
+Color loginReminderColor = new Color(0xff999999);
---
+++ lib/screenSizeGetters.dart
@@ -0,0 +1,14 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+double height;
+double width;
+
+class ScreenSizes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+  }
+}
