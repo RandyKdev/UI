7f834f27c4fa5b20411639b4298ac3dcf845afc6
ECOMAN003 <ekiliheman@gmail.com>
2020-09-23 18:23:44 +0100

    ht12eddf234ik,dcfgf6y54yg7u

--- lib/screenSizeGetters.dart
+++ lib/screenSizeGetters.dart
@@ -6,7 +6,6 @@ double width;
 class ScreenSizes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
   }
 }
